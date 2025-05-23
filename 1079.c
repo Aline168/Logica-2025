1788dc1bd8ab081dbf4f0231c45b1ca92981bb28
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-05-23 14:07:32 -0300

    Create 1079.c

---
+++ 1079.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+int N;
+double x, y, z, media;
+
+scanf("%d", &N);
+
+for (int i = 0; i < N; i++) {
+    scanf("%lf %lf %lf", &x, &y, &z);
+    media = (x*2 + y*3 + z*5) / 10;
+    printf("%.1lf\n", media);
+}
+
+return 0;
+}
