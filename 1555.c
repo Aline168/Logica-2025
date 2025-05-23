722c95d53d7175044b5df115a05389adb681c130
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-05-23 14:05:55 -0300

    Create 1555.c

---
+++ 1555.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+int N;
+int x, y;
+
+scanf("%d", &N);
+
+for (int i = 0; i < N; i++) {
+    scanf("%d %d", &x, &y);
+
+    double r = pow(3 * x, 2) + pow(y, 2);
+    double b = 2 * pow(x, 2) + pow(5 * y, 2);
+    double c = -100 * x + pow(y, 3);
+
+    if (r > b && r > c) {
+        printf("Rafael ganhou\n");
+    } else if (b > r && b > c) {
+        printf("Beto ganhou\n");
+    } else {
+        printf("Carlos ganhou\n");
+    }
+}
+
+return 0;
+}
