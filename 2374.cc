12862c85baf4a92d9a4e7588859dc15d605cec1d
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-06-27 16:58:12 -0300

    Create 2374.cc

---
+++ 2374.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int M=0;
+int N=0;
+
+    cin >> N;
+    cin >> M;
+
+
+    if (N<=40 || M <=40){
+    cout << N-M << endl;
+
+    }
+    else {
+        cout<< "valor invalido";
+    }
+
+    return 0;
+}
