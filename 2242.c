04c902c7022270d321d0fb79f96371ed9a1f5ad8
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-07-02 10:40:55 -0300

    Update 2242.c

--- 2242.c
+++ 2242.c
@@ -1,28 +1,35 @@
+
 #include <stdio.h>
 #include <string.h>
 
 #define MAX 51
 
-int eh_vogal(char c) {
+int eh_vogal(char c)
+{
     return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
 }
 
-int main() {
+int main()
+{
     char risada[MAX], vogais[MAX];
     int v = 0;
 
     fgets(risada, MAX, stdin);
     risada[strcspn(risada, "\n")] = '\0'; // remove \n
 
-    for (int i = 0; risada[i] != '\0'; i++) {
-        if (eh_vogal(risada[i])) {
+    for (int i = 0; risada[i] != '\0'; i++)
+    {
+        if (eh_vogal(risada[i]))
+        {
             vogais[v++] = risada[i];
         }
     }
 
     int engracada = 1;
-    for (int i = 0; i < v / 2; i++) {
-        if (vogais[i] != vogais[v - 1 - i]) {
+    for (int i = 0; i < v / 2; i++)
+    {
+        if (vogais[i] != vogais[v - 1 - i])
+        {
             engracada = 0;
             break;
         }
