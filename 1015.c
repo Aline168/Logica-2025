5c6e1361896bbb2effb82f8556a23c61f6ca3689
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-07-02 10:32:14 -0300

    Update 1015.c

--- 1015.c
+++ 1015.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
+int main()
+{
 
-double  x1, x2;
-double y1, y2;
-double distancia;
+    double  x1, x2;
+    double y1, y2;
+    double distancia;
 
-scanf("%lf %lf", &x1, &y1);
-scanf("%lf %lf", &x2, &y2);
+    scanf("%lf %lf", &x1, &y1);
+    scanf("%lf %lf", &x2, &y2);
 
-distancia = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    distancia = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 
 
-printf("%.4lf\n", distancia);
+    printf("%.4lf\n", distancia);
 
-return 0;
+    return 0;
+}
