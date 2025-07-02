d3b4df3de4c5817ca1d0c988203763fb96d0666c
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-07-02 10:38:23 -0300

    Update 1255.c

--- 1255.c
+++ 1255.c
@@ -7,20 +7,19 @@
 int main() {
     int n;
     char linha[MAX];
-    int freq[26]; // Frequência de 'a' a 'z'
-
+    int freq[26]; 
     scanf("%d", &n);
-    getchar(); // Consome o '\n' após o número
+    getchar(); 
 
     for (int t = 0; t < n; t++) {
         fgets(linha, MAX, stdin);
 
-        // Zerar o vetor de frequência
+        
         for (int i = 0; i < 26; i++) {
             freq[i] = 0;
         }
 
-        // Contar a frequência das letras
+       
         for (int i = 0; linha[i] != '\0'; i++) {
             if (isalpha(linha[i])) {
                 char c = tolower(linha[i]);
@@ -28,14 +27,14 @@ int main() {
             }
         }
 
-        // Achar a maior frequência
+       
         int maxFreq = 0;
         for (int i = 0; i < 26; i++) {
             if (freq[i] > maxFreq)
                 maxFreq = freq[i];
         }
 
-        // Imprimir as letras com maior frequência
+       
         for (int i = 0; i < 26; i++) {
             if (freq[i] == maxFreq)
                 printf("%c", 'a' + i);
