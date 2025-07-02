c01cb4375ec086c73197da00041038c1791cc80b
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-07-02 10:22:10 -0300

    Update 1001.c

--- 1001.c
+++ 1001.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int main() {
-int A, B, X;
+int main()
+{
+    int A, B, X;
 
-// Lê dois inteiros da entrada
-scanf("%d", &A);
-scanf("%d", &B);
+    scanf("%d", &A);
+    scanf("%d", &B);
 
-// Soma os dois valores
-X = A + B;
 
-// Imprime o resultado no formato exigido
-printf("X = %d\n", X);
+    X = A + B;
 
-return 0;
+
+    printf("X = %d\n", X);
+
+    return 0;
 }
