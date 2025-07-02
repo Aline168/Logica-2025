6a60c7d7e75c27d57799e0199d65c593506cd3ed
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-07-02 10:34:06 -0300

    Update 1079.c

--- 1079.c
+++ 1079.c
@@ -1,16 +1,19 @@
+
 #include <stdio.h>
 
-int main() {
-int N;
-double x, y, z, media;
+int main()
+{
+    int N;
+    double x, y, z, media;
 
-scanf("%d", &N);
+    scanf("%d", &N);
 
-for (int i = 0; i < N; i++) {
-    scanf("%lf %lf %lf", &x, &y, &z);
-    media = (x*2 + y*3 + z*5) / 10;
-    printf("%.1lf\n", media);
-}
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%lf %lf %lf", &x, &y, &z);
+        media = (x*2 + y*3 + z*5) / 10;
+        printf("%.1lf\n", media);
+    }
 
-return 0;
+    return 0;
 }
