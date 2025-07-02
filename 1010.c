88d832968247d96bbd9b08c1b61229c57b7a5b03
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-07-02 10:31:35 -0300

    Update 1010.c

--- 1010.c
+++ 1010.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 
-int c1, c2;
-float n1, n2;
-float v1, v2;
-float soma;
-
-
-scanf("%d %f %f", &c1, &n1, &v1);
-scanf("%d %f %f", &c2, &n2, &v2);
-
-soma = (n2*v2) + (n1 * v1);
+    int c1, c2;
+    float n1, n2;
+    float v1, v2;
+    float soma;
 
 
-printf("VALOR A PAGAR: R$ %.2f\n", soma);
+    scanf("%d %f %f", &c1, &n1, &v1);
+    scanf("%d %f %f", &c2, &n2, &v2);
 
-return 0;
+    soma = (n2*v2) + (n1 * v1);
+
+
+    printf("VALOR A PAGAR: R$ %.2f\n", soma);
+
+    return 0;
 }
