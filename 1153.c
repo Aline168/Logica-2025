397eaf7d8f4735678da4d1ef3ff903cba3d7bb18
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-07-02 10:19:56 -0300

    Update 1153.c

--- 1153.c
+++ 1153.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int main() {
-int n, fatorial = 1;
+int main(){
+    int n, fatorial = 1;
 
-scanf("%d", &n);
+    scanf("%d", &n);
 
-for (int i = 1; i <= n; i++) {
-    fatorial = fatorial * i;
-}
+    for (int i = 1; i <= n; i++)
+    {
+        fatorial = fatorial * i;
+    }
 
-printf("%d\n", fatorial);
+    printf("%d\n", fatorial);
 
-return 0;
+    return 0;
 }
